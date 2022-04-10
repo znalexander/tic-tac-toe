bcac1a728dd1eb84fa6dda32b9cbf3641cf82638
Aleksandr <a.zakharov.n@gmail.com>
2022-04-10 12:28:56 +0300

    Implement DataPrinter component

--- src/alexander/zakharov/study/java/tictactoe/component/DataPrinter.java
+++ src/alexander/zakharov/study/java/tictactoe/component/DataPrinter.java
@@ -17,6 +17,7 @@
 
 package alexander.zakharov.study.java.tictactoe.component;
 
+import alexander.zakharov.study.java.tictactoe.model.Cell;
 import alexander.zakharov.study.java.tictactoe.model.GameTable;
 
 /**
@@ -26,10 +27,23 @@ import alexander.zakharov.study.java.tictactoe.model.GameTable;
 public class DataPrinter {
 
     public void printMappingTable() {
-
+        System.out.println("-------------");
+        System.out.println("| 7 | 8 | 9 |");
+        System.out.println("-------------");
+        System.out.println("| 4 | 5 | 6 |");
+        System.out.println("-------------");
+        System.out.println("| 1 | 2 | 3 |");
+        System.out.println("-------------");
     }
 
     public void printGameTable(final GameTable gameTable) {
-
+        for (int i = 0; i < 3; i++) {
+            System.out.println("-------------");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("| " + gameTable.getSing(new Cell(i, j)) + ' ');
+            }
+            System.out.println('|');
+        }
+        System.out.println("-------------");
     }
 }
